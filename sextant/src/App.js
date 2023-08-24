aa9c4db453c8c27656683c43f4024f918e87e0a4
Israel-Temiloluwa <isanif32@gmail.com>
2023-08-24 21:32:15 +0100

    third commit

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './App.css';
 import Banner from './Components/Banner/Banner';
 import Exhibit from './Components/Exhibit/Exhibit';
+import AddressDisplay from './Components/AddressDisplay';
 
 function App(){
 return (
     <div className="App">
         <Banner/>
-        <Exhibit name="I'm the exhibit!"></Exhibit>
-        <Exhibit name="I'm another exhibit!"></Exhibit>
-        <Exhibit name="I'm an exhibit too!"></Exhibit>
-        <Exhibit name="Don't forget about me, I'm an exhibit too!"></Exhibit>
+        <Exhibit name="Public IPv4 Address">
+          <AddressDisplay url='https://api.ipify.org?format=json' />
+        </Exhibit>
+        <Exhibit name="Public IPv6 Address">
+          <AddressDisplay url='https://api64.ipify.org?format=json' />
+        </Exhibit>
     </div>
 );
 }
