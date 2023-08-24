a4bfaf3b53538b3804cf0616437fc376b588c708
Israel-Temiloluwa <isanif32@gmail.com>
2023-08-24 21:52:41 +0100

    my fourth commit

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import Exhibit from './Components/Exhibit/Exhibit';
 import AddressDisplay from './Components/AddressDisplay';
+import PylonConnector from './Components/PylonCollector';
 
 function App(){
 return (
@@ -13,6 +14,9 @@ return (
         </Exhibit>
         <Exhibit name="Public IPv6 Address">
           <AddressDisplay url='https://api64.ipify.org?format=json' />
+        </Exhibit>
+        <Exhibit name="Pylon Packet Latency">
+          <PylonConnector />
         </Exhibit>
     </div>
 );
